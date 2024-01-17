f1f584575bd079f50cbe1beff6d00af3080f4240
DarkJoker360 <simoespo159@gmail.com>
2024-01-17 20:58:45 +0530

    display: Disable backpressure prop
    - Helps fix janks and lags.
    
    ref: https://github.com/VoltageOS/frameworks_native/commit/42bc4bf8e046274f84610cf6e13f042fac1ee93d
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Change-Id: Ia77597aa8ec455bd80fc86fa8113e56b1ca29b73
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- config/display-product.mk
+++ config/display-product.mk
@@ -95,7 +95,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.high_fps_late_sf_phase_offset_ns=-2000000 \
     debug.sf.high_fps_early_phase_offset_ns=-4000000 \
     debug.sf.high_fps_early_gl_phase_offset_ns=-2000000 \
-    debug.sf.enable_gl_backpressure=1
+    debug.sf.disable_backpressure=1
 
 # Enable offline rotator for Bengal.
 ifneq ($(TARGET_BOARD_PLATFORM),bengal)
