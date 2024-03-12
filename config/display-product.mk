49bd2810f213a320c0ad6eb1b728bdc24882c9a9
mtsxxx <162270661+mtsxxx@users.noreply.github.com>
2024-03-13 00:48:29 +0700

    display: Remove debug.sf.disable_client_composition_cache 1
    Because it causes jank when pulling up quick settings.
    
    Change-Id: d488473dc844f0ce7187641fc18c00860c4bb89f
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- config/display-product.mk
+++ config/display-product.mk
@@ -95,7 +95,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.high_fps_late_sf_phase_offset_ns=-2000000 \
     debug.sf.high_fps_early_phase_offset_ns=-4000000 \
     debug.sf.high_fps_early_gl_phase_offset_ns=-2000000 \
-    debug.sf.disable_client_composition_cache=1 \
     debug.sf.enable_gl_backpressure=1
 
 # Enable offline rotator for Bengal.
